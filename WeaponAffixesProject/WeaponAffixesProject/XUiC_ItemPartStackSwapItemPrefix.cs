0487797031c71f4f98554328584dc0c70a31803c
Thomas Vos <thomasjvos12@gmail.com>
2026-03-08 17:30:36 +0100

    0.4.3

---
+++ WeaponAffixesProject/WeaponAffixesProject/XUiC_ItemPartStackSwapItemPrefix.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace WeaponAffixesProject
+{
+    [HarmonyPatch(typeof(XUiC_ItemPartStack), nameof(XUiC_ItemPartStack.SwapItem))]
+    public static class XUiC_ItemPartStackSwapItem
+    {
+        private static bool Prefix(XUiC_ItemPartStack __instance)
+        {
+            if (AffixUtils.IsAffixMod(__instance.itemClass))
+                return false;
+
+            return true;
+        }
+
+    }
+}
